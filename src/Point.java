8a09a7d7084037b644a3ea3e06c5a28690cdc92f
chonrc <chonrc00@estudiantes.unileon.es>
2021-10-06 01:20:28 -0700

    Añadida la práctica finalizada

---
+++ src/Point.java
@@ -0,0 +1,19 @@
+public class Point {
+	private int x;
+	private int y;
+	
+	public Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void moveTo(int newX, int newY){
+		this.x = newX;
+		this.y = newY;
+	}
+	
+	public String toString(){
+		return "(" + this.x + "," + this.y + ")";
+	}
+	
+}
---
+++ src/Square.java
@@ -0,0 +1,23 @@
+public class Square {
+	private int size;
+	private Point origin;
+	
+	public Square(int size, Point origin){
+		this.size = size;
+		this.origin = origin;
+	}
+	
+	public int getSize(){
+		return this.size;
+	}
+	
+	public Point getOrigin(){
+		return this.origin;
+	}
+	
+	public String toString(){
+		return "Square: Size = " + this.size +
+				" Origin = " + this.origin;
+	}
+	
+}
